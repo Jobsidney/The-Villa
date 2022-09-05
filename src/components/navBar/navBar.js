52a53951f09750ded37e6270a4d4f772ff7733d5
Jobsidney <jobsidney67@gmail.com>
2022-09-05 18:16:49 +0300

    Villa logo

--- src/components/navBar/navBar.js
+++ src/components/navBar/navBar.js
@@ -1,10 +1,13 @@
 import React from 'react'
+import Back1Svg from './Back1Svg'
 
 function NavBar() {
   return (
     <div>
       <nav>
-        <label  class="logo">Villa</label>
+        <label  class="logo">
+          <Back1Svg/>
+        </label>
         <ul class="nav-link">
             <li><a href="#home">Home</a></li>
             <li><a href="#about">About</a></li>
