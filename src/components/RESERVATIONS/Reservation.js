45764c5a80032ef3c02cc61bddb53fc449c762a9
Jobsidney <jobsidney67@gmail.com>
2022-09-05 23:49:27 +0300

    Reservation css

--- src/components/RESERVATIONS/Reservation.js
+++ src/components/RESERVATIONS/Reservation.js
@@ -1,14 +1,23 @@
 import React from 'react'
 import Button from '../menu/Button'
 import NavBar from '../navBar/navBar'
+import reservation from './reserve.css'
 
 function Reservation() {
   return (
     <div>
-        <div class="containerTop">
+        <div className="containerTop">
         <div className='overlay'>
             <NavBar classN={"navbar"}/>
+            
         </div>
+        <div class="shouting">
+        <div class="">Reservations</div>
+        <div class="subHero">Our venues can also be booked for 
+            events ranging from small, to large. The reservations must be 
+            made a week in advance.
+        </div>
+    </div>
         </div> 
         
         
