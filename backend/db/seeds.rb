24932192b142475a901d7982771a46ad1e029940
Jobsidney <jobsidney67@gmail.com>
2022-09-07 00:31:22 +0300

    FIles

---
+++ backend/db/seeds.rb
@@ -0,0 +1,13 @@
+
+puts "🌱 Seeding spices..."
+
+Classroom.create(name: "Lauryn Philipa Wescott")
+Classroom.create(name: "Dinah Elenora North")
+Classroom.create(name: "Nikkole Gertrude Yap")
+Classroom.create(name: "Jackie Kaety Comstock")
+Classroom.create(name: "Macy Parnel Davis")
+Classroom.create(name: "")
+Department.create()
+Teacher.create()
+
+puts "✅ Done seeding!"
