28a83fd33d0af7f7874792dc99da3ba5b9cff956
Jobsidney <jobsidney67@gmail.com>
2022-09-06 11:37:03 +0300

    Breakfast menu UI

--- src/components/MENU/Desserts/Dessert.js
+++ src/components/MENU/Desserts/Dessert.js
@@ -2,21 +2,23 @@ import React from 'react'
 import dessert from './dessert.css'
 
 function Dessert() {
+
+    function DessertCard(){
+        return(
+            <div class="text1">
+                <h3>Key Lime pie <span>$8.00</span></h3>
+                <p>Tangy Custard with Graham Cracker 
+                    crust. Wripped cream and lime zest
+                </p>
+            </div>
+        ) }
+
+
   return (
     <div class="main-content">
         <div class="main-text">
             <h1>Desserts</h1>
-            <div class="text1">
-                    <h3>Key Lime pie <span>$8</span></h3>
-                    <p>Tangy Custard with Graham Cracker 
-                        crust. Wripped cream and lime zest
-                    </p>
-            </div>
-            <div class="text1">
-                    <h3>Newyork cheesecake <span>$8.55</span></h3>
-                    <p>Topped with caramel, chocolate curls and cocoa powder
-                    </p>
-                </div>
+            <DessertCard/>
 
         </div>
         <div class="image">
--- src/components/MENU/Menu.js
+++ src/components/MENU/Menu.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Button from '../menu/Button'
+import BreakFast from './Breakfast/BreakFast'
 import QuickDrinks from './cocktails/QuickDrinks'
 import Dessert from './Desserts/Dessert'
 import Lunch from './Lunch/Lunch'
@@ -12,15 +13,14 @@ function Menu() {
     <div class="main-menu">
 
 <div class="main-links">
-    <h4>The Digi Main Menu</h4>
+    <h4>The Villa Main Menu</h4>
     <h1>Today's Special</h1>
     <div class="links">
         <ul>
             <li><a href="../../index.html" className="home">Home</a></li>
             <li><a href="/" className="active">Quick</a></li>
             <li><a href="../components/breakfast/breakfast.html">Breakfast</a></li>
-            <li><a href="../components/lunch/lunch.html">Lunch</a></li>
-            <li><a href="../components/dinner/dinner.html">Dinner</a></li>
+            <li><a href="../components/lunch/lunch.html">Lunch/ Dinner</a></li>
             <li><a href="../components/dessert/dessert.html">Dessert</a></li>
         </ul>
         <div className="button">
@@ -30,7 +30,8 @@ function Menu() {
 </div>
 {/* <QuickDrinks/> */}
 {/* <Lunch/> */}
-<Dessert/>
+{/* <Dessert/> */}
+<BreakFast/>
 </div>
   )
 }
