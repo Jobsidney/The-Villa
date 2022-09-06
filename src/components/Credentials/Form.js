1ab0b10995e2373bd6fe5b4f7eb8aa863c8ca99e
Jobsidney <jobsidney67@gmail.com>
2022-09-06 16:40:28 +0300

    Sign in complete POST form

--- src/components/Credentials/Form.js
+++ src/components/Credentials/Form.js
@@ -1,13 +1,14 @@
 import React from 'react'
 import Login from './Login'
 import login from './login.css'
+import SignUp from './SignUp'
 function Form() {
   return (
     <div className="image">
         <div className='overlay'>
           <div className="all">
-            
-          <Login/>
+
+          <SignUp/>
 
           </div>
          </div>
--- src/components/Credentials/SignUp.js
+++ src/components/Credentials/SignUp.js
@@ -1,4 +1,4 @@
-import React from 'react'
+
 import React, { useState, useEffect } from 'react';
 
 function SignUp() {
@@ -10,16 +10,29 @@ const [formData,setData] = useState({})
       ...formData,
       [name]: value,
     })
-
-
   }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    console.log(formData);
+
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(formData),
+    })
+  }
+
+
   return (
-    <form action="">
+    <form action="" onSubmit={handleSubmit}>
         <h2 className="heading">Villa</h2>         
-        <input type="email" name="email" id="username-field" className="login-button" placeholder="Email"></input>
-        <input type="password" name="password" id="password-field" className="login-button" placeholder="Set Password"></input>
-        <input type="password" name="password-confirm" id="password-field" className="login-button" placeholder="Confirm Password"></input>
-        <button type='submit' classNameName='log' id='logIn'>Login</button>
+        <input type="email" name="email" id="username-field" className="login-button" placeholder="Email" onChange={handleChange}></input>
+        <input type="password" name="password" id="password-field" className="login-button" placeholder="Set Password" onChange={handleChange}></input>
+        <input type="password" name="password-confirm" id="password-field" className="login-button" placeholder="Confirm Password" onChange={handleChange}></input>
+        <button type='submit' className='log' id='logIn'>Login</button>
     </form>
   )
 }
