83d090ffe7d4404b0a84a1d060f3e202f403ed41
Jobsidney <jobsidney67@gmail.com>
2022-09-07 17:55:53 +0300

    models correction

---
+++ backend/app/models/food.rb
@@ -0,0 +1,5 @@
+class Food < ActiveRecord::Base
+    has_many :reviews
+    has_many :users, through: :reviews
+    
+end
---
+++ backend/app/models/review.rb
@@ -0,0 +1,4 @@
+class Review < ActiveRecord::Base
+    belongs_to :food
+    belongs_to :user
+end
