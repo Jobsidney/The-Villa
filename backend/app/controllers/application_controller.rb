539c43bd83d3cee4b13c35e4376a5ee31afa41ca
Jobsidney <jobsidney67@gmail.com>
2022-09-08 11:49:26 +0300

    sinatra fetch API

--- backend/app/controllers/application_controller.rb
+++ backend/app/controllers/application_controller.rb
@@ -2,13 +2,38 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/" do
-    { message: "Good luck with your proje!" }.to_json
+  # get "/" do
+  #   { message: "Good luck with your proje!" }.to_json
+  # end
+
+
+  get "/users" do
+    users=User.all.to_json
+    users
   end
+
+  get "/reviews" do
+    Review.all.to_json
+  end
+
   get "/foods" do
-    users=User.all
-    puts users
-    users.to_json
+      Food.all.to_json
+  end
+
+
+
+  #users data API
+  get "/users" do
+    User.all.to_json
+  end
+
+  post "/users" do
+    user=User.create(
+      full_Name: params[:full_name],
+      email: params[:email],
+      password: params[:password],
+      password_Confirming: params[:password_confirming],
+    )
   end
 
 end
--- backend/app/models/category.rb
+++ backend/app/models/category.rb
@@ -1,3 +1,4 @@
 class FoodCategory < ActiveRecord::Base
     has_many :foods
+    # has_many :reviews, through: :foods
 end
